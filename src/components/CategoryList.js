139b7786cf2cd814bcfc1b28275cb54b7f04fe1b
Malvina52 <malbihi@gmail.com>
2023-06-26 19:36:04 -0300

    nuevo

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 const CategoryList = () => {
-    const categories = [
+  const categories = [
     { value: 'category1', label: 'Categoría 1' },
     { value: 'category2', label: 'Categoría 2' },
     { value: 'category3', label: 'Categoría 3' },
@@ -11,23 +13,33 @@ const CategoryList = () => {
       options: [
         { value: 'familia', label: 'Familia' },
         { value: 'laboral', label: 'Laboral' },
-        { value: 'daños', label: 'Daños' }
-      ]
-    }
+        { value: 'daños', label: 'Daños' },
+      ],
+    },
   ];
+
   return (
-    <div className="category-list"style={{ backgroundColor: 'black', paddingTop: '1rem', width: '100%', position: 'fixed', top: 0, left: 0 }}>
+    <div className="category-list" style={{ backgroundColor: 'black', paddingTop: '1rem', width: '100%', position: 'fixed', top: 0, left: 0 }}>
       {categories.map((category, index) => (
-          <a key={index}
-              href={`/categoria/${category}`} className="category-link"
-    style={{ color: 'blue',
-            fontFamily: 'Arial',
-            paddingRight: '1cm',
-            display: 'inline-block',
-            marginTop: '2rem' }}
-         >
-              {category}
-          </a>
+        <div key={index}>
+          {category.options ? (
+            <Select options={category.options} />
+          ) : (
+            <a
+              href={`/categoria/${category.value}`}
+              className="category-link"
+              style={{
+                color: 'blue',
+                fontFamily: 'Arial',
+                paddingRight: '1cm',
+                display: 'inline-block',
+                marginTop: '2rem',
+              }}
+            >
+              {category.label}
+            </a>
+          )}
+        </div>
       ))}
     </div>
   );
